bbdd45374d22a01a2d0e89c2ed891e239f4daaed
HuseyinKla <huseyin.kla1@gmail.com>
2025-03-20 19:29:01 +0300

    bakımdayız eklendi

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './App.css';
 import logo from './logo.png';
 
+
+//logo rengi: #470e0d
 function App() {
   return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <main className="App-main">
-          <h1>ANA SAYFAYA HOŞ GELDİNİZ.</h1>
-        </main>
+          <header className="App-header" style={{}}>
+              <img src={logo} className="App-logo" alt="logo"/>
+              <h1 style={{alignSelf: 'center', flex: 1, color: 'white', marginLeft: -50}}>ŞU ANDA BAKIMDAYIZ</h1>
+          </header>
+          <main className="App-main">
+              <img src={logo} style={{height: 400}} alt="logo"/>
+              <h1 style={{alignSelf: 'center', flex: 1, color: '#470e0d'}}>EN KISA SÜREDE HİZMETİNİZE AÇILACAĞIZ</h1>
+
+          </main>
       </div>
   );
 }
